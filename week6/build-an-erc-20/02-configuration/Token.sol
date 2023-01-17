545c1a05fa11579154a3f143adddcb23d98c4e3e
Abraham Grullon <104178527+CoffeeNbourbon@users.noreply.github.com>
2023-01-17 16:45:34 -0500

    Update Token.sol

--- week6/build-an-erc-20/02-configuration/Token.sol
+++ week6/build-an-erc-20/02-configuration/Token.sol
@@ -4,6 +4,6 @@ pragma solidity 0.8.17;
 contract Token {
     uint public totalSupply;
     uint8 public decimals = 18;
-    string public name = "Rupee";
-    string public symbol = "PKR";
+    string public name = "Bourbon";
+    string public symbol = "BBN";
 }
